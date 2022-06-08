4b444c61673a9da6fa5b7ab30ce26a139d099aa3
MSG-EE <nkosimasegow@gmail.com>
2022-06-08 23:50:05 +0200

    c fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,9 @@
  * _islower -> checking for lowercases 
  * Return: Returns 1 and 0 depending on condition
  */
+
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z')
+	return (c >= 'a' && c <= 'z');
+		
 }

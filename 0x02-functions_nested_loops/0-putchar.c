54be503b703e1a95e2540ced2e979e6c05ea9bcd
MSG-EE <nkosimasegow@gmail.com>
2022-06-08 13:22:46 +0200

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
-/**
-* this ia a script that prints putchar
-* 
+#include "main.h"
+
+/** main - programm that prints putchar
+* Return: always 0
 */
-main(void)
+int main(void)
 {
-	printf("_putchar");
-	return(0);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
 }

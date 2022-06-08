f698214e709f054f911cb73a7884c081043aa91d
MSG-EE <nkosimasegow@gmail.com>
2022-06-08 13:04:39 -0700

    c void committed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * printing alphabets in lowercase sentence
+ * print_alphabet
  * the Betty style way
  * return : a to z
 */
+
 void print_alphabet(void);
 {
 	int j;
 	for (j = 'a'; j <= 'z'; j++)
 	{
-		_putchar (j);
+		_putchar(j);
 	}
 	_putchar('\n')
 }

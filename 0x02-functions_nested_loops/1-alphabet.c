ced0570c97dea3e76e0d8e64af8087ce26d30c5e
MSG-EE <nkosimasegow@gmail.com>
2022-06-08 12:54:04 -0700

    c void committed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  * the Betty style way
  * return : a to z
 */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	int j;
 	for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar (j);
 	}
-	_putchar ('\n')
+	_putchar('\n')
 }
